62a9e6e1eeed994dfdcf620ae57958b02958bc60
Yuri Alves <yuri.o.alves2@outlook.com>
2021-01-30 02:15:50 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
+import db.Seller;
 import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
      
-		Department obj = new Department(1,"Games");
+		Department department = new Department(1,"Games");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(01, "CJ", "CJ@gmail.com", new Date(), 3000.0, department);
+		
+		System.out.println(seller);
 
 	}
 
